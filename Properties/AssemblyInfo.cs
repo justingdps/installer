0ba085c71ca71e6e571e721f478dcdbf38f024fe
Justin Pridgen <justincpridgen672@gmail.com>
2021-07-14 12:29:59 -0400

    removed unnecessary stuff

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -7,12 +6,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("JustinGDPS Installer")]
 [assembly: AssemblyDescription("JustinGDPS Installer")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BWBJustin")]
 [assembly: AssemblyProduct("JustinGDPS Installer")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
